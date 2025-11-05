f944b92fff414716d461378ca64adba2e9ea7675
Marcos Aleixo <maknytz@gmail.com>
2025-11-05 12:44:41 -0300

    Add descrição ao produto

--- GestaoDeEstoque/Models/Produto.cs
+++ GestaoDeEstoque/Models/Produto.cs
@@ -9,6 +9,7 @@ namespace GestaoDeEstoque.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string Descricao { get; set; }
         public float Preco { get; set; }
         public int Quantidade { get; set; }
     }
